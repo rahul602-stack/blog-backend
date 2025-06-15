0c7cc4ebe8787f19576bc64cbee1c6002b514b3a
rahul602-stack <rahulsurya879@gmail.com>
2025-06-16 01:30:52 +0200

    Deleted HQ Report files and cleaned up backend

--- src/routes/hqReportRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getHQSalesReport } from '../controllers/hqReportController';
-
-const router = express.Router();
-
-router.get('/sales', getHQSalesReport);
-
-export default router;
